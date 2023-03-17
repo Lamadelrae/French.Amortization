49222d1124a1fe77458a0cd400be843abab57739
Matthew Almeida <matthew.almeida572@gmail.com>
2023-03-17 03:43:02 -0300

    kubernetes config

--- French.Amortization.Api/Program.cs
+++ French.Amortization.Api/Program.cs
@@ -12,12 +12,8 @@ builder.Services.AddMediatR(config =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
